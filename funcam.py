d625a1b0889d9274490d90085779f8019f80608e
meltsi <49880156+meltsi@users.noreply.github.com>
2021-06-14 08:42:24 +0200

    Update funcam.py

--- funcam.py
+++ funcam.py
@@ -17,12 +17,31 @@ if not cap.isOpened(): # if not open already
 cap.set(3, 640)
 cap.set(4, 480)
 
+
+
+
+
+
+
+
 alternate_background= cv2.imread("pink.png")#todo 
 alternate_background=cv2.cvtColor(alternate_background, cv2.COLOR_BGRA2RGB)
+rval, frame = cap.read()
+height,width, channels = frame.shape
+#resize background to frame
+alternate_background = cv2.resize(alternate_background, (width,height))
 
 
 
-
+counter=2
+counterprev=1 
+#initialize array for saving current mean of all frames
+meanimage=np.zeros((height,width,3), np.uint8)
+framecopy=np.zeros((height,width,3))
+#make copy of meanimage for calculations outside of rgb color space
+meanimagecopy=np.zeros((height,width,3))
+#initialize meanimage with first frame
+meanimage=frame
 
 
 
@@ -60,14 +79,6 @@ def ChromaKeyPlusDespill(frame4):
     
     #apply despill on frame
     frame4=np.where(despill3==1,((frame_r+frame_b)/2),frame4)
-    
-    
-    
-    
-    
-    #todo
-    framecopy=np.zeros((width,height,3))
-    framecopy[:,:,:]=frame4[:,:,:]
     
     
     
@@ -145,8 +156,7 @@ def ChromaKeyVersion1a(frame4):
     chroma_key=cv2.cvtColor( np.uint8([[colorbase]] ), cv2.COLOR_RGB2YUV)[0][0]
     
     
-    framecopy=np.zeros((width,height,3))
-    framecopy[:,:,:]=frame4[:,:,:]
+    
     frame4[:,:,:]=np.minimum(255,2*(np.maximum(0,frame4[:,:,:]-((1/2)*chroma_key))))
     
     
@@ -182,7 +192,7 @@ def ChromaKeyVersion1a(frame4):
     
    
     #assign pixels with distances<threshold to foreground and >=threshold to foreground
-    threshold1=60
+    threshold1=100
     threshold2=160
     distances= np.where(distances<threshold1,0,1)
     #distances= np.where(((distances-threshold1)/(threshold2/threshold1))<=0,0, np.where(((distances-threshold1)/(threshold2/threshold1))>=1,1, 3*((distances-threshold1)/(threshold2/threshold1))**2- 2*((distances-threshold1)/(threshold2/threshold1))**3))
@@ -213,7 +223,7 @@ def updatemeanimage():
     counterplusone()#update counters
 
 
-def ChromaKeyVersion1b():
+def ChromaKeyVersion1b(frame4):
     #array that returns the result
     resultimage= np.zeros((width,height,3), np.uint8)
     
@@ -231,9 +241,9 @@ def ChromaKeyVersion1b():
     updatemeanimage()
     
     #assign pixels with distances<threshold to foreground and >=threshold to foreground
-    resultimage[:,:,0]=((distances[:,:])*frame4[:,:,0])+((1-distances[:,:])*background[:,:,0])
-    resultimage[:,:,1]=((distances[:,:])*frame4[:,:,1])+((1-distances[:,:])*background[:,:,1])
-    resultimage[:,:,2]=((distances[:,:])*frame4[:,:,2])+((1-distances[:,:])*background[:,:,2])
+    resultimage[:,:,0]=((distances[:,:])*frame4[:,:,0])+((1-distances[:,:])*alternate_background[:,:,0])
+    resultimage[:,:,1]=((distances[:,:])*frame4[:,:,1])+((1-distances[:,:])*alternate_background[:,:,1])
+    resultimage[:,:,2]=((distances[:,:])*frame4[:,:,2])+((1-distances[:,:])*alternate_background[:,:,2])
     
     return(resultimage)
     
@@ -295,9 +305,11 @@ def makemask(frame4):
 
 
 
-#Chroma Key 1c
+#Chroma Key 1c-----------------------------------------------------------------------------------
 
-#Chroma Key 2
+
+#Chroma Key 2-------------------------------------------------------------------------------------
+#todo desc
 def ChromaKeyVersion2(frame4):
     #array that returns the result
     resultimage= np.zeros((width,height,3), np.uint8)
@@ -319,7 +331,7 @@ def ChromaKeyVersion2(frame4):
     
     #copy Cr and Cb chanel for calculating distance outside of uint8
     ycrcb_cr=np.zeros((width,height))
-    ycrcb_cr[:,:]=ycrcr[:,:,1]
+    ycrcb_cr[:,:]=ycrcb[:,:,1]
     ycrcb_cb=np.zeros((width,height))
     ycrcb_cb[:,:]=ycrcb[:,:,2]
     
@@ -340,19 +352,20 @@ def ChromaKeyVersion2(frame4):
     cv2.filter2D(distances,-1, kernel)
     
     #assign pixels with distances<threshold to foreground and >=threshold to foreground
-    threshold1=90
+    threshold1=120
     distances= np.where(distances<threshold1,0,1)
-    resultimage[:,:,0]=((distances[:,:])*frame4[:,:,0])+((1-distances[:,:])*background[:,:,0])
-    resultimage[:,:,1]=((distances[:,:])*frame4[:,:,1])+((1-distances[:,:])*background[:,:,1])
-    resultimage[:,:,2]=((distances[:,:])*frame4[:,:,2])+((1-distances[:,:])*background[:,:,2])
+    resultimage[:,:,0]=((distances[:,:])*frame4[:,:,0])+((1-distances[:,:])*alternate_background[:,:,0])
+    resultimage[:,:,1]=((distances[:,:])*frame4[:,:,1])+((1-distances[:,:])*alternate_background[:,:,1])
+    resultimage[:,:,2]=((distances[:,:])*frame4[:,:,2])+((1-distances[:,:])*alternate_background[:,:,2])
     return(resultimage)
 
 
 
 
 #Chroma Key 3
-#Chroma key hsv version
-def ChromaKeyVersion3():
+#todo desc
+#todo fix
+def ChromaKeyVersion3(frame4):
     #array that returns the result
     resultimage= np.zeros((width,height,3), np.uint8)
     
@@ -394,25 +407,128 @@ def ChromaKeyVersion3():
     cv2.filter2D(distances,-1, kernel)
     
     #assign pixels with distances<threshold to foreground and >=threshold to foreground
-    threshold1=90
+    threshold1=150
     distances= np.where(distances<threshold1,0,1)
-    resultimage[:,:,0]=((distances[:,:])*frame[:,:,0])+((1-distances[:,:])*background[:,:,0])
-    resultimage[:,:,1]=((distances[:,:])*frame[:,:,1])+((1-distances[:,:])*background[:,:,1])
-    resultimage[:,:,2]=((distances[:,:])*frame[:,:,2])+((1-distances[:,:])*background[:,:,2])
+    resultimage[:,:,0]=((distances[:,:])*frame4[:,:,0])+((1-distances[:,:])*alternate_background[:,:,0])
+    resultimage[:,:,1]=((distances[:,:])*frame4[:,:,1])+((1-distances[:,:])*alternate_background[:,:,1])
+    resultimage[:,:,2]=((distances[:,:])*frame4[:,:,2])+((1-distances[:,:])*alternate_background[:,:,2])
     return(resultimage)
 
 #Color Key
 
-#Luma Key 1
+def ColorKey(frame4):
+    #array that returns the result
+    resultimage= np.zeros((width,height,3), np.uint8)
 
-#Luma Key 2
+    #green RGB(0,255,0)
+    color_key=np.array([0,255,0])
+    
+    #make copy of chroma_key for calculations outside of uint8
+    color_key2=np.array([float(color_key[0]),float(color_key[1]),float(color_key[2])])
+        
+    #array that saves the euclidean distance between all pixels in current frame and chroma key
+    distances=np.zeros((width,height))    
+    
+    #
+    rgb_r=np.zeros((width,height))
+    rgb_r[:,:]=frame4[:,:,0]
+    rgb_g=np.zeros((width,height))
+    rgb_g[:,:]=frame4[:,:,1]
+    rgb_b=np.zeros((width,height))
+    rgb_b[:,:]=frame4[:,:,2]
+    
+    
+    #calculate distances
+    distances=((color_key2[0]-rgb_r)**2+(color_key2[1]-rgb_g)**2+(color_key2[2]-rgb_b)**2)**(0.5)
+        
+    #    
+    threshold1=180
+    distances= np.where(distances<threshold1,0,1)
+    
+    resultimage[:,:,0]=((distances[:,:])*frame4[:,:,0])+((1-distances[:,:])*alternate_background[:,:,0])
+    resultimage[:,:,1]=((distances[:,:])*frame4[:,:,1])+((1-distances[:,:])*alternate_background[:,:,1])
+    resultimage[:,:,2]=((distances[:,:])*frame4[:,:,2])+((1-distances[:,:])*alternate_background[:,:,2])
+    return(resultimage)
+
+#Luma Key 1------------------------------------------------------------------------------------
+
+#todo add source
+def smoothstep0(luminance):
+    threshold1=220
+    threshold2=250
+    return smoothstep(luminance[0],threshold1,threshold2)
+
+def smoothstep(x, xmin, xmax):
+    smoothstep1=(x - xmin) / (xmax - xmin)
+    if smoothstep1 <= 0:
+        return 0
+    elif smoothstep1 >= 1:
+        return 1
+    else:
+        return 3*smoothstep1**2- 2*smoothstep1**3
 
 
 
+    
+def LumaKey(frame4):
+    lumaMin=220
+    lumaMax=250
+    lumaMinSmooth=0
+    lumaMaxSmooth=0
 
 
+    #array that returns the result
+    resultimage= np.zeros((width,height,3), np.uint8)
+
+    #convert rgb value to luminance
+    luma_converter=np.array([0.2989, 0.5870, 0.1140])
+    luminance=np.zeros((width,height,1))
+    luminance[:,:,0]=frame4[:,:,0]*luma_converter[0]+frame4[:,:,1]*luma_converter[1]+frame4[:,:,2]*luma_converter[2]
+    
+    #mask
+    mask=np.apply_along_axis(smoothstep0,2,luminance)
+  
+    
+    #assign resulting image to foreground or background
+    resultimage[:,:,0]=(1-mask[:,:])*frame4[:,:,0]+(mask[:,:])*alternate_background[:,:,0]
+    resultimage[:,:,1]=(1-mask[:,:])*frame4[:,:,1]+(mask[:,:])*alternate_background[:,:,1]
+    resultimage[:,:,2]=(1-mask[:,:])*frame4[:,:,2]+(mask[:,:])*alternate_background[:,:,2]
+    return(resultimage)
+
+
+#Luma Key 2 ----------------------------------------------------------------------------------
+
+
+def LumaKeyv2(frame4):
+    threshold=130
+    tolerance=10
+
+    #array that returns the result
+    resultimage= np.zeros((width,height,3), np.uint8)
+
+    #convert every pixel in frame from rgb to yuv
+    yuv = cv2.cvtColor(frame4, cv2.COLOR_RGB2YUV)
+    
+    luminance=np.zeros((width,height,1))
+    luminance[:,:,0]=yuv[:,:,0]
+
+    mask=np.where((0<luminance),np.where((220>luminance),1,0),1)
+    
+    #assign resulting image to foreground or background
+    resultimage[:,:,0]=(mask[:,:,0])*frame4[:,:,0]+(1-mask[:,:,0])*alternate_background[:,:,0]
+    resultimage[:,:,1]=(mask[:,:,0])*frame4[:,:,1]+(1-mask[:,:,0])*alternate_background[:,:,1]
+    resultimage[:,:,2]=(mask[:,:,0])*frame4[:,:,2]+(1-mask[:,:,0])*alternate_background[:,:,2]
+
+
+    return(resultimage)
 
 #--------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
 while(cap.isOpened()):
     try:
         ret, frame = cap.read()
@@ -422,19 +538,26 @@ while(cap.isOpened()):
             # Now: The fun stuff! ------------------------------------------
             #shape of frame
             width, height, channels = frame4.shape
-    
+            
+            #initialize counters for number of frames (for mean-picture)   
+
+
+
+
+
             #make empty mask with same size as frame
             mask= np.zeros((width,height), np.uint8)
 
             
             #apply filter
-            #frame4=ChromaKeyVersion1a(frame)
-            #ChromaKeyVersion2()
-            #ChromaKeyVersion3()
-            #ColorKeyVersion1()
-            #ColorKeyVersion2()
-            #LumaKeyv3()
-            frame4=ChromaKeyPlusDespill(frame4)
+	    frame4=ChromaKeyPlusDespill(frame4)
+            #frame4=ChromaKeyVersion1a(frame4)
+            #frame4=ChromaKeyVersion1b(frame4)
+            #frame4=ChromaKeyVersion2(frame4)
+            #frame4=ChromaKeyVersion3(frame4)
+            #frame4=ColorKey(frame4)
+            #frame4=LumaKey(frame4)
+            #frame4=LumaKeyv2(frame4)
 
             # End of fun stuff ... --------------------------------------------
 
